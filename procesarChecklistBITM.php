81f3fb014ff1ba2f85ee593eec4fef5a049d61fd
julio contreras <julio.contreras.insuco@gmail.com>
2017-01-12 13:41:43 -0300

    Se quitan comentarios y get defined vars

--- procesarChecklistBITM.php
+++ procesarChecklistBITM.php
@@ -41,124 +41,11 @@ if (isset($_POST['estadoChecklist']) && $_POST['estadoChecklist'] != '') {
         settype(trim($bitm_acreditadoPorCertificadoConadiPapel), "integer");
         settype(trim($bitm_acreditadoPorCertificadoConadiEmp), "integer");
 
-        /**
-         *
-         * $bitm_regionDeEjecucion;
-         * $bitm_rutAlumno;
-         * $bitm_nombreAlumno;
-         * $bitm_domicilioAlumno;
-         * $bitm_tipoBecaAlumno;
-         * $bitm_nivelEducacional;
-         * $bitm_provinciaDomicilioFamiliarAlumno;
-         * $bitm_comunaDomicilioFamiliarAlumno;
-         * $bitm_tipoUnidadOperativa;
-         * $bitm_institucionEvaluadora;
-         * $bitm_profesionalQueEvaluo;
-         * $bitm_rbd;
-         *
-         *
-         * $bitm_nombreEstEducacionalPapel;
-         * $bitm_nombreEstEducacionalSinab;
-         * $bitm_nombreEstEducacionalEmp;
-         * $bitm_resultadoDigiNombreEstEducacional;
-         * $bitm_ObsDigiNombreEstEducacional;
-         *
-         *
-         * $bitm_cursoBasicaMediaPapel;
-         * $bitm_cursoBasicaMediaSinab;
-         * $bitm_cursoBasicaMediaEmp;
-         * $bitm_resultadoEduBasicaMedia;
-         * $bitm_ObsEduBasicaMedia;
-         *
-         *
-         * $bitm_discapacidadPapel;
-         * $bitm_discapacidadSinab;
-         * $bitm_discapacidadEmp;
-         * $bitm_resultadoDigiDiscapacidad;
-         * $bitm_ObsDigiDiscapacidad;
-         *
-         *
-         * $bitm_puebloIndigenaPapel;
-         * $bitm_puebloIndigenaSinab;
-         * $bitm_puebloIndigenaEmp;
-         * $bitm_resultadoDigiPuebloIndigena;
-         * $bitm_ObsDigiPuebloIndigena;
-         *
-         *
-         * $bitm_acreditadoPorCertificadoConadiPapel;
-         * $bitm_acreditadoPorCertificadoConadiSinab;
-         * $bitm_acreditadoPorCertificadoConadiEmp;
-         * $bitm_resultadoDigiAcreditadoPorCertificadoConadi;
-         * $bitm_ObsDigiAcreditadoPorCertificadoConadi;
-         *
-         *
-         * $bitm_acreditadoPorApellidoPapel;
-         * $bitm_acreditadoPorApellidoSinab;
-         * $bitm_acreditadoPorApellidoEmp;
-         * $bitm_resultadoDigiAcreditadoPorApellido;
-         * $bitm_ObsDigiAcreditadoPorApellido;
-         *
-         *
-         * $bitm_notaPapel;
-         * $bitm_notaSinab;
-         * $bitm_notaEmp;
-         * $bitm_resultadoDigiNota;
-         * $bitm_ObsDigiNota;
-         *
-         *
-         * $bitm_deficitApoyoFamiliarPapel;
-         * $bitm_deficitApoyoFamiliarSinab;
-         * $bitm_deficitApoyoFamiliarEmp;
-         * $bitm_resultadoDigiDeficitApoyoFamiliar;
-         * $bitm_ObsDigiDeficitApoyoFamiliar;
-         *
-         *
-         * $bitm_duplicidadFuncionesAlumnoPapel;
-         * $bitm_duplicidadFuncionesAlumnoSinab;
-         * $bitm_duplicidadFuncionesAlumnoEmp;
-         * $bitm_resultadoDigiDuplicidadFuncionesAlumno;
-         * $bitm_ObsDigiDuplicidadFuncionesAlumno;
-         *
-         *
-         * $bitm_hermanosHijosEstudiantesPapel;
-         * $bitm_hermanosHijosEstudiantesSinab;
-         * $bitm_hermanosHijosEstudiantesEmp;
-         * $bitm_resultadoDigiHermanosHijosEstudiantes;
-         * $bitm_ObsDigiHermanosHijosEstudiantes;
-         *
-         *
-         * $bitm_aislamientoPromedioComunalPapel;
-         * $bitm_aislamientoPromedioComunalSinab;
-         * $bitm_aislamientoPromedioComunalEmp;
-         * $bitm_resultadoDigiAislamientoPromedioComunal;
-         * $bitm_ObsDigiAislamientoPromedioComunal;
-         *
-         *
-         * $bitm_aislamientoDeLocalidadesPapel;
-         * $bitm_aislamientoDeLocalidadesSinab;
-         * $bitm_aislamientoDeLocalidadesEmp;
-         * $bitm_resultadoDigiAislamientoDeLocalidades;
-         * $bitm_ObsDigiAislamientoDeLocalidades;
-         *
-         *
-         * $bitm_lugarEstudioAlumnoPapel;
-         * $bitm_lugarEstudioAlumnoSinab;
-         * $bitm_lugarEstudioAlumnoEmp;
-         * $bitm_resultadoDigiLugarEstudioAlumno;
-         * $bitm_ObsDigiLugarEstudioAlumno;
-         *
-         *
-         * $bitm_codEstadoChecklist;
-         * $bitm_estadoChecklist;
-         * $bitm_guardarBITM;
-         *
-         */
-
+    
         #Variables antecedentes
         $query = "CALL BITM_SUP_InsertarDatos(" . $idRevisor . "',
         
-        '" . $bitm_regionDeEjecucion . "', '" . $bitm_estadoChecklist . "', '" . $bitm_codEstadoChecklist . "',
-        '" . $rutAlumno . "', " . "'" . $dvAlumno . "', '".$bitm_resultadoRutAlumno."', '".$bitm_obsRutAlumno."',
+        '" . $bitm_regionDeEjecucion . "', '" . $bitm_estadoChecklist . "', '" . $bitm_codEstadoChecklist . "','" . $rutAlumno . "', " . "'" . $dvAlumno . "', '".$bitm_resultadoRutAlumno."', '".$bitm_obsRutAlumno."',
         '" . $bitm_nombreAlumno . "','" . $bitm_resultadoNombreAlumno."', '".$bitm_obsNombreAlumno."',
         '" . $bitm_domicilioAlumno . "','" . $bitm_tipoBecaAlumno . "',
         '" . $bitm_nivelEducacional . "', '" . $bitm_provinciaDomicilioFamiliarAlumno . "',
@@ -193,10 +80,4 @@ if (isset($_POST['estadoChecklist']) && $_POST['estadoChecklist'] != '') {
 
 
 }
-?>
-<pre>
-<?php
-print_r(get_defined_vars());//
-?>
-</pre>
 
