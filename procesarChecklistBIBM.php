3c9ae9744ff251381909e54da768ebb4127ebe72
julio contreras <julio.contreras.insuco@gmail.com>
2017-01-12 12:42:30 -0300

    JCM Se ordena a partir de formulario

--- procesarChecklistBIBM.php
+++ procesarChecklistBIBM.php
@@ -139,27 +139,37 @@ if (isset($_POST['estadoChecklist']) && $_POST['estadoChecklist'] != '') {
     settype(trim($bibm_acreditadoPorCertificadoConadiEmp), "integer");
 
 
-    $query = "CALL BIBM_SUP_InsertarDatos(" . $idRevisor . ", '" . $bibm_regionDeEjecucion . "', '" . $bibm_estadoChecklist . "', '" . $bibm_codEstadoChecklist . "', '" . $rutAlumno . "', "
-        . "'" . $dvAlumno . "', '" . $bibm_nombreAlumno . "', "
-        . "'" . $bibm_domicilioAlumno . "', '" . $bibm_tipoBecaAlumno . "', '" . $bibm_nivelEducacional . "', '" . $bibm_provinciaDomicilioFamiliarAlumno . "', '" . $bibm_comunaDomicilioFamiliarAlumno . "', "
-        . "'" . $bibm_profesionalQueEvaluo . "', '" . $bibm_tipoUnidadOperativa . "', '" . $bibm_institucionEvaluadora . "', '" . $bibm_rbd . "', " . "'" . $bibm_puebloIndigenaPapel . "', "
-        . "'" . $bibm_puebloIndigenaSinab . "','" . $bibm_puebloIndigenaEmp . "','" . $bibm_resultadoDigiPuebloIndigena . "', '" . $bibm_acreditadoPorCertificadoConadiPapel . "', '" . $bibm_acreditadoPorCertificadoConadiSinab . "', "
-        . "'" . $bibm_acreditadoPorCertificadoConadiEmp . "', '" . $bibm_resultadoDigiAcreditadoPorCertificadoConadi . "', '" . $bibm_acreditadoPorApellidoPapel . "', '" . $bibm_acreditadoPorApellidoSinab . "', "
-        . "'" . $bibm_acreditadoPorApellidoEmp . "', '" . $bibm_resultadoDigiAcreditadoPorApellido . "',  " . "'" . $bibm_notaPapel . "', '" . $bibm_notaSinab . "', '" . $bibm_notaEmp . "', '" . $bibm_resultadoDigiNota . "',  "
-        . "'" . $bibm_deficitApoyoFamiliarPapel . "', '".$bibm_deficitApoyoFamiliarSinab."','" . $bibm_deficitApoyoFamiliarEmp . "', '" . $bibm_resultadoDigiDeficitApoyoFamiliar . "', "
-        . "'" . $bibm_discapacidadPapel . "', '" . $bibm_discapacidadSinab . "', '" . $bibm_discapacidadEmp . "', '" . $bibm_resultadoDigiDiscapacidad . "','" . $bibm_duplicidadFuncionesAlumnoPapel . "', "
-        . "'" . $bibm_duplicidadFuncionesAlumnoSinab . "', '" . $bibm_duplicidadFuncionesAlumnoEmp . "', '" . $bibm_resultadoDigiDuplicidadFuncionesAlumno . "','" . $bibm_hermanosHijosEstudiantesPapel . "', "
-        . "'" . $bibm_hermanosHijosEstudiantesSinab . "', '" . $bibm_hermanosHijosEstudiantesEmp . "', '" . $bibm_resultadoDigiHermanosHijosEstudiantes . "', '" . $bibm_participaEnOrgIndigenaPapel . "', "
-        . "'" . $bibm_participaEnOrgIndigenaSinab . "', '" . $bibm_participaEnOrgIndigenaEmp . "', '" . $bibm_resultadoDigiParticipacionOrgIndigena . "','" . $bibm_seDomiciliaViveEnComunidadIndigenaPapel . "', "
-        . "'" . $bibm_seDomiciliaViveEnComunidadIndigenaSinab . "', '" . $bibm_seDomiciliaViveEnComunidadIndigenaEmp . "', '" . $bibm_resultadoDigiSeDomiciliaViveEnComunidadIndigena . "' ,"
-        . "'" . $bibm_participaDePracticasCulturalesRitualesDeLaComunidadPapel . "', '" . $bibm_participaDePracticasCulturalesRitualesDeLaComunidadSinab . "', '" . $bibm_participaDePracticasCulturalesRitualesDeLaComunidadEmp . "', "
-        //Agregados
-        . "'" . $bibm_nombreEstEducacionalPapel . "', '" . $bibm_nombreEstEducacionalSinab . "', '" . $bibm_nombreEstEducacionalEmp . "', '" . $bibm_resultadoDigiNombreEstEducacional . "'"
-        . "'" . $bibm_cursoBasicaMediaPapel . "', '" . $bibm_cursoBasicaMediaSinab . "', '" . $bibm_cursoBasicaMediaEmp . "', '" . $bibm_resultadoEduBasicaMedia . "'"
-        //Termino de agregados
-        . "'" . $bibm_resultadoDigiParticipaDePracticasCulturalesRitualesDeLaComunidad . "', '" . $bibm_aislamientoPromedioComunalPapel . "', '" . $bibm_aislamientoPromedioComunalSinab . "', " . "'" . $bibm_aislamientoPromedioComunalEmp . "', '" . $bibm_resultadoDigiAislamientoPromedioComunal . "', "
-        . "'" . $bibm_lugarEstudioAlumnoPapel . "', '" . $bibm_lugarEstudioAlumnoSinab . "', " . "'" . $bibm_lugarEstudioAlumnoEmp . "', '" . $bibm_resultadoDigiLugarEstudioAlumno . "', '" . $bibm_certificadoParticipacionOrgIndigena . "', '" . $bibm_certificadoSeDomiciliaViveEnComunidadIndigena . "', "
-        . "'" . $bibm_certificadoParticipaDePracticasCulturalesRitualesDeLaComunidad . "', '" . $bibm_certificadoConadiAlumno . "', " . "'" . $bibm_certificadoEmbarazo . ")";
+    $query = "CALL BIBM_SUP_InsertarDatos(" . $idRevisor . ", 
+    #Variables generales
+    
+    '" . $bibm_regionDeEjecucion . "', '" . $bibm_estadoChecklist . "', '" . $bibm_codEstadoChecklist . "',
+    '" . $rutAlumno . "', " . "'" . $dvAlumno . "', '" . $bibm_nombreAlumno . "', " . "'" . $bibm_domicilioAlumno . "',
+    '" . $bibm_tipoBecaAlumno . "', '" . $bibm_nivelEducacional . "', '" . $bibm_provinciaDomicilioFamiliarAlumno . "',
+    '" . $bibm_comunaDomicilioFamiliarAlumno . "', " . "'" . $bibm_profesionalQueEvaluo . "', '" . $bibm_tipoUnidadOperativa . "',
+    '" . $bibm_institucionEvaluadora . "', '" . $bibm_rbd . "', " . "
+    
+   
+    #Variables normales
+    
+    '" . $bibm_nombreEstEducacionalPapel . "', '" . $bibm_nombreEstEducacionalSinab . "', '" . $bibm_nombreEstEducacionalEmp . "', '" . $bibm_resultadoDigiNombreEstEducacional . "',
+    '" . $bibm_cursoBasicaMediaPapel . "', '" . $bibm_cursoBasicaMediaSinab . "', '" . $bibm_cursoBasicaMediaEmp . "', '" . $bibm_resultadoEduBasicaMedia . "',
+    '" . $bibm_discapacidadPapel . "', '" . $bibm_discapacidadSinab . "', '" . $bibm_discapacidadEmp . "', '" . $bibm_resultadoDigiDiscapacidad . "',
+    '" . $bibm_puebloIndigenaPapel . "', " . "'" . $bibm_puebloIndigenaSinab . "','" . $bibm_puebloIndigenaEmp . "','" . $bibm_resultadoDigiPuebloIndigena . "',
+    '" . $bibm_acreditadoPorCertificadoConadiPapel . "', '" . $bibm_acreditadoPorCertificadoConadiSinab . "', " . "'" . $bibm_acreditadoPorCertificadoConadiEmp . "','" . $bibm_resultadoDigiAcreditadoPorCertificadoConadi . "',
+    '" . $bibm_acreditadoPorApellidoPapel . "', '" . $bibm_acreditadoPorApellidoSinab . "', " . "'" . $bibm_acreditadoPorApellidoEmp . "', '" . $bibm_resultadoDigiAcreditadoPorApellido . "',
+    '" . $bibm_notaPapel . "', '" . $bibm_notaSinab . "', '" . $bibm_notaEmp . "', '" . $bibm_resultadoDigiNota . "',
+    '" . $bibm_deficitApoyoFamiliarPapel . "', '".$bibm_deficitApoyoFamiliarSinab."','" . $bibm_deficitApoyoFamiliarEmp . "', '" . $bibm_resultadoDigiDeficitApoyoFamiliar . "',
+    '" . $bibm_duplicidadFuncionesAlumnoPapel . "', " . "'" . $bibm_duplicidadFuncionesAlumnoSinab . "', '" . $bibm_duplicidadFuncionesAlumnoEmp . "', '" . $bibm_resultadoDigiDuplicidadFuncionesAlumno . "',
+    '" . $bibm_hermanosHijosEstudiantesPapel . "', " . "'" . $bibm_hermanosHijosEstudiantesSinab . "', '" . $bibm_hermanosHijosEstudiantesEmp . "', '" . $bibm_resultadoDigiHermanosHijosEstudiantes . "',
+    '" . $bibm_participaEnOrgIndigenaPapel . "', " . "'" . $bibm_participaEnOrgIndigenaSinab . "', '" . $bibm_participaEnOrgIndigenaEmp . "', '" . $bibm_resultadoDigiParticipacionOrgIndigena . "',
+    '" . $bibm_seDomiciliaViveEnComunidadIndigenaPapel . "', " . "'" . $bibm_seDomiciliaViveEnComunidadIndigenaSinab . "', '" . $bibm_seDomiciliaViveEnComunidadIndigenaEmp . "', '" . $bibm_resultadoDigiSeDomiciliaViveEnComunidadIndigena . "' ,
+    '" . $bibm_participaDePracticasCulturalesRitualesDeLaComunidadPapel . "', '" . $bibm_participaDePracticasCulturalesRitualesDeLaComunidadSinab . "', '" . $bibm_participaDePracticasCulturalesRitualesDeLaComunidadEmp . "','" . $bibm_resultadoDigiParticipaDePracticasCulturalesRitualesDeLaComunidad . "',
+    '" . $bibm_aislamientoPromedioComunalPapel . "', '" . $bibm_aislamientoPromedioComunalSinab . "', " . "'" . $bibm_aislamientoPromedioComunalEmp . "', '" . $bibm_resultadoDigiAislamientoPromedioComunal . "',
+    '" . $bibm_lugarEstudioAlumnoPapel . "', '" . $bibm_lugarEstudioAlumnoSinab . "', " . "'" . $bibm_lugarEstudioAlumnoEmp . "', '" . $bibm_resultadoDigiLugarEstudioAlumno . "',  
+     
+     #Documental
+     
+     '" . $bibm_certificadoParticipacionOrgIndigena . "', '" . $bibm_certificadoSeDomiciliaViveEnComunidadIndigena . "', " . "'" . $bibm_certificadoParticipaDePracticasCulturalesRitualesDeLaComunidad . "', '" . $bibm_certificadoConadiAlumno . "', " . "'" . $bibm_certificadoEmbarazo . ")";
 
 
     //$result = mysqli_query($conexion, $query) or die(mysqli_error($conexion));
